9901c9e4e4820df937ba8603f96d413038ad6d37
darkcite <qqq.darkcite@gmail.com>
2023-10-05 15:03:49 -0500

    + new version

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,37 @@
 <template>
-  <div>
+  <div class="container">
     <button v-if="!isAuthenticated" @click="loginWithMetaMask">
       <img src="@/assets/metamask-icon.svg" alt="MetaMask Logo" width="32">
       Login with MetaMask
     </button>
-    <button v-if="!isAuthenticated" @click="loginWithWalletConnect">
-      <img src="@/assets/walletconnect-logo.svg" alt="WalletConnect Logo" width="32">
-      Login with WalletConnect
-    </button>
 
     <div v-if="isAuthenticated">
-      <div>
-        <select v-model="selectedCountry">
-          <option value="">Select starting currency</option>
-          <option v-for="country in countries" :key="country" :value="country">{{ country }}</option>
-        </select>
-
-        <table v-if="selectedCountry && exchangeRates[selectedCountry]">
-          <thead>
-            <tr>
-              <th>Child entity</th>
-              <th>EUR</th>
-              <th>USD</th>
+      <table>
+        <thead>
+          <tr>
+            <th>Fiat Buy</th>
+            <th>Buy Bank</th>
+            <th>Buy Price</th>
+            <th>Fiat Sell</th>
+            <th>Sell Bank</th>
+            <th>Sell Price</th>
+            <th>Profit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <template v-for="fiat in Object.keys(arbitrageData)">
+            <tr v-for="(entry, index) in Object.values(arbitrageData[fiat])" :key="index">
+              <td>{{ entry.fiat_buy }}</td>
+              <td>{{ entry.buy_bank }}</td>
+              <td>{{ entry.buy_price }}</td>
+              <td>{{ entry.fiat_sell }}</td>
+              <td>{{ entry.sell_bank }}</td>
+              <td>{{ entry.sell_price }}</td>
+              <td>{{ entry.percentage_difference_usd }}%</td>
             </tr>
-          </thead>
-          <tbody>
-            <tr v-for="childEntity in Object.keys(exchangeRates[selectedCountry])" :key="childEntity">
-              <td>{{ childEntity }}</td>
-              <td v-if="exchangeRates[selectedCountry][childEntity]['EUR']">{{
-                exchangeRates[selectedCountry][childEntity]['EUR'] }}</td>
-              <td v-else>-</td>
-              <td v-if="exchangeRates[selectedCountry][childEntity]['USD']">{{
-                exchangeRates[selectedCountry][childEntity]['USD'] }}</td>
-              <td v-else>-</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+          </template>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -44,49 +39,19 @@
 <script>
 import axios from 'axios';
 import Web3 from 'web3';
-import WalletConnectProvider from "@walletconnect/web3-provider";
 
 export default {
   name: 'App',
 
   data() {
     return {
-      selectedCountry: '',
-      countries: [],
-      exchangeRates: {},
+      arbitrageData: {},
       isAuthenticated: false,
-      web3: null,
+      web3: null
     };
   },
 
   methods: {
-    async loginWithWalletConnect() {
-      try {
-        const provider = new WalletConnectProvider({
-          rpc: { 1: "https://mainnet.infura.io/v3/4f9c9233fa9e45a582f7883b3968e5f2" },  // replace YOUR_INFURA_PROJECT_ID
-        });
-
-        await provider.enable();
-
-        this.web3 = new Web3(provider);
-
-        const accounts = await this.web3.eth.getAccounts();
-
-        if (accounts && accounts.length > 0) {
-          this.isAuthenticated = true;
-          // Optionally: Check if the connected account address has the right permissions on your backend
-        }
-      } catch (error) {
-        if (error.message === "User closed modal") {
-          // Handle the error, e.g., show a message or simply do nothing
-          console.log("User closed the WalletConnect modal.");
-        } else {
-          // Handle other errors
-          console.error("An error occurred:", error);
-        }
-      }
-    },
-
     async loginWithMetaMask() {
       if (typeof window.ethereum !== 'undefined') {
         try {
@@ -105,13 +70,12 @@ export default {
     },
 
     fetchData() {
-      axios.get('https://us-central1-arbit-400006.cloudfunctions.net/get_sum')
+      axios.get('https://us-central1-arbit-400006.cloudfunctions.net/get_arbitrage_data')
         .then(response => response.data)
         .then(data => {
-          this.countries = Object.keys(data);
-          this.exchangeRates = data;
+          this.arbitrageData = data;
         });
-    },
+    }
   },
 
   mounted() {
@@ -121,62 +85,77 @@ export default {
   },
 };
 </script>
-<style scoped>
-body,
-h1,
-h2,
-h3,
-p,
-button {
+
+<style>
+/* Global styles */
+html, body {
+  height: 100%;
   margin: 0;
   padding: 0;
-  font-family: Arial, sans-serif;
+  background-color: #121212; /* Dark background */
+  color: #c5c5c5; /* Light gray text color for better visibility on dark background */
 }
 
-div {
+/* Resetting defaults */
+h1, h2, h3, p, button, table, th, td {
+  margin: 0;
+  padding: 0;
+  font-family: 'Courier New', Courier, monospace; /* Classic hacker font */
+  border: none;
+}
+
+.container {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
-  background: #f5f5f5;
+  width: 100%;
+  min-height: 100vh; /* This ensures the container takes up the full viewport height */
+  background-color: #121212; /* Set dark background for container as well */
 }
 
 button {
   display: flex;
   align-items: center;
+  justify-content: center;
   gap: 8px;
-  margin: 10px;
   padding: 8px 16px;
-  background: #111;
-  color: #fff;
-  border: none;
+  background-color: #333;
+  color: #0f0; /* Classic green text */
+  border: 1px solid #0f0; /* Green border */
   border-radius: 4px;
   cursor: pointer;
-  transition: background 0.2s;
+  transition: background-color 0.2s, transform 0.2s;
 }
 
 button:hover {
-  background: #333;
+  background-color: #555;
+  transform: scale(1.05); /* Slightly enlarge the button on hover */
 }
 
 table {
   margin-top: 20px;
   width: 80%;
   border-collapse: collapse;
+  box-shadow: 0px 0px 10px rgba(0, 255, 0, 0.4); /* Greenish shadow */
 }
 
 thead {
-  background: #333;
-  color: #fff;
+  background-color: #222; /* Slightly lighter than the background */
 }
 
-th,
-td {
-  padding: 8px 12px;
-  border: 1px solid #ddd;
+th, td {
+  padding: 10px 15px;
+  border-bottom: 1px solid #0f0; /* Green borders */
+}
+
+tbody tr:last-child td {
+  border-bottom: none;
 }
 
 tbody tr:hover {
-  background: #eee;
-}</style>
+  background-color: #1e1e1e; /* A shade lighter on hover */
+}
+</style>
+
+
